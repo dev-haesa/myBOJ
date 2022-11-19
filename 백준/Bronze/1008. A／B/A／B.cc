a61a4d6220db4d517d161911243739229f422441
dev-haesa <86779559+dev-haesa@users.noreply.github.com>
2022-11-19 19:26:16 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long double a, b;
+    cin >> a >> b;
+    cout.precision(12);
+    cout << fixed;
+    cout << a/b;
+    return 0;
+}
